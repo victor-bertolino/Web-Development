c462eb976433f32af74165f54478368aea43e23c
Victor Bertolino <victorbertolino.pro@gmail.com>
2023-08-16 13:23:52 -0300

    Create dados-3.js

---
+++ fundamentos/inicio/dados-3.js
@@ -0,0 +1,11 @@
+let idade = 31;
+console.log(typeof 31);
+console.log(typeof idade);
+
+let salario = 454.12
+console.log(typeof salario);
+
+let estaChovendo = true; // ou false
+console.log(typeof estaChovendo);
+
+console.log(typeof  "Teste");
