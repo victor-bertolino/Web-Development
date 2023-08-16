ec39ffa8f90df064f645dd1128a734d0f3b3d4bb
Victor Bertolino <victorbertolino.pro@gmail.com>
2023-08-16 13:56:03 -0300

    Update desafio-troca.js

---
+++ fundamentos/inicio/desafio-troca.js
@@ -0,0 +1,13 @@
+let a = 7;
+let b = 94;
+let troca;
+
+troca = a;
+a = b;
+b = troca;
+
+// [a, b] = [b, a];
+
+// trocar... a = 94 e b = 7
+console.log("Valor de A: " + a);
+console.log("Valor de B: " + b); 
