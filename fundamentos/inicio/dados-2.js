c3bba9e5f23201695d4021dedf58d829861651ea
Victor Bertolino <victorbertolino.pro@gmail.com>
2023-08-16 13:00:35 -0300

    Create dados-2.js

---
+++ fundamentos/inicio/dados-2.js
@@ -0,0 +1,14 @@
+ let preco = 19.90;
+ let desconto = 0.4;
+
+ console.log(19.90 * 0.6);
+
+ let precoComDesconto = preco * (1 - desconto)
+ console.log(precoComDesconto);
+ 
+ let nome = "Caderno";
+ let categoria = "Papelaria";
+ console.log("Produto: " + nome + ", Cateogira: " + categoria + ", Preço: " + precoComDesconto);
+
+ console.log(1 + 1);
+ console.log("1" + 1)
