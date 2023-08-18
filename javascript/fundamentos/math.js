55af2d2373bb59d430653dbf58b0cee28834dc21
Victor Bertolino <victorbertolino.pro@gmail.com>
2023-08-18 19:03:24 -0300

    Update math.js

--- javascript/fundamentos/math.js
+++ javascript/fundamentos/math.js
@@ -1,5 +1,8 @@
-const raio = 5.6
-const area = Math.PI * Math.pow(raio, 2)
+// const raio = 5.6
+// const area = Math.PI * Math.pow(raio, 2)
 
-console.log(area.toFixed(2))
-console.log(typeof Math)
+// console.log(area.toFixed(2))
+// console.log(typeof Math)
+
+var numero = 2
+Math.pow(numero, 3)
