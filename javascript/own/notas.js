a8490759f2e68c15e77f6edbf3d021c678a7045e
Victor Bertolino <victorbertolino.pro@gmail.com>
2023-08-19 23:39:55 -0300

    Create notas.js

---
+++ javascript/own/notas.js
@@ -0,0 +1,8 @@
+var nomeAluno = "Victor Bertolino"
+var nota1 = 6
+var nota2 = 5
+var nota3 = 7
+var nota4 = 8
+var media = (nota1 + nota2 + nota3 + nota4) / 4
+
+console.log("A nota média do aluno " + nomeAluno + " é " + media)
