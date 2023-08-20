71c56fd865f1953cfbe0124b0ff3850b5ecaacd9
Victor Bertolino <victorbertolino.pro@gmail.com>
2023-08-20 10:00:33 -0300

    Create aula.js

---
+++ javascript/dio/classes_e_objetos/aula.js
@@ -0,0 +1,19 @@
+class Pessoa {
+    nome;
+    idade;
+
+    descrever() {
+
+    }
+}
+
+const vitor = new Pessoa();
+vitor.nome = 'Vitor J Guerra';
+vitor.idade = 25;
+
+const renan = new Pessoa();
+vitor.nome = 'Renan J Paula';
+vitor.idade = 30;
+
+console.log(vitor);
+console.log(renan);
