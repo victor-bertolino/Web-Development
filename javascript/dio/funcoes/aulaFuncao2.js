79d636b20f9a0b490aef5b5e4d25dee4f1cba32f
Victor Bertolino <victorbertolino.pro@gmail.com>
2023-08-20 06:24:35 -0300

    Create aulaFuncao2.js

---
+++ javascript/dio/funcoes/aulaFuncao2.js
@@ -0,0 +1,9 @@
+function verificarIdade (idade){
+    if (idade < 18) {
+        console.log('Menor de Idade');
+    } else {
+        console.log('Maior de idade');
+    }
+}
+
+verificarIdade(12);
