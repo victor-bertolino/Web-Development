3ccdd9e35283e7b12e88c349b0b713e7df65feee
Victor Bertolino <victorbertolino.pro@gmail.com>
2023-08-20 10:00:42 -0300

    Create funcao.js

---
+++ javascript/yt/funcao.js
@@ -0,0 +1,19 @@
+function soma(){
+      12 + 42;
+}
+
+console.log(soma);
+
+function realParaDolar(real, cotacaoDolar){
+    return real * cotacaoDolar;
+}
+
+console.log(realParaDolar(10,5.55))
+
+function paraCelsius (valorFahrenheit) {
+return (5/9) * (valorFahrenheit-32)
+}
+
+let x = paraCelsius(77);
+
+console.log (x)
