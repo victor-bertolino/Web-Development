97d1058244757c5491a3a1d072d398216189bfb5
Victor Bertolino <victorbertolino.pro@gmail.com>
2023-08-21 18:13:46 -0300

    Create aula.js

---
+++ javascript/dio/listas(arrays)/aula.js
@@ -0,0 +1,11 @@
+const notas = [];
+
+notas.push(5);
+notas.push(6);
+notas.push(5);
+notas.push(8);
+notas.push(9);
+
+const soma = notas[0] + notas[1] + notas[2] + notas[3] + notas[4] + 
+
+console.log(soma / 5)
