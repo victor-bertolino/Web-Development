08e8e399aa8237e24c73f4144f9edb7fd10cffc0
Victor Bertolino <victorbertolino.pro@gmail.com>
2023-08-21 18:13:35 -0300

    Create aula.js

---
+++ javascript/dio/arraysEfor/aula.js
@@ -0,0 +1,17 @@
+const notas = [];
+
+notas.push(5);
+notas.push(6);
+notas.push(5);
+notas.push(8);
+notas.push(9);
+
+let soma = 0;
+
+for (let i = 0; i < notas.length; i++) {
+    const nota = notas[i];
+    soma = soma + nota;
+}
+
+const media = soma / notas.length
+console.log(media);
