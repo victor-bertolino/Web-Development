8bff7cfdb0ef4756dc953ce94fe58621911b6298
Victor Bertolino <victorbertolino.pro@gmail.com>
2023-08-21 18:13:37 -0300

    Create ex4.js

---
+++ javascript/dio/exercícios/ex4.js
@@ -0,0 +1,7 @@
+const multiplicador = 5;
+const tabuada = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 1; i < tabuada.length+1; i++) {
+    const tabuada = [i];
+    console.log(multiplicador + "x" + tabuada + " é: " + (multiplicador * tabuada));
+};
