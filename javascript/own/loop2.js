12a6cdb9d0fcef4759c2390c2d415364537e0400
Victor Bertolino <victorbertolino.pro@gmail.com>
2023-08-22 17:49:19 -0300

    Create loop2.js

---
+++ javascript/own/loop2.js
@@ -0,0 +1,8 @@
+const nome = "Victor Bertolino";
+
+for (let i = 0; i < nome.length; i++) {
+    const letra = nome[i];
+    if (letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u") {
+console.log(letra);
+    };
+};
