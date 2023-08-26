16493c0f5787683339bebe683f649512b816a7e7
Victor Bertolino <victorbertolino.pro@gmail.com>
2023-08-25 23:04:32 -0300

    Update teste.js

--- javascript/dio/teste.js
+++ javascript/dio/teste.js
@@ -1,22 +1,11 @@
-const {gets, print} = require("./exercícios/funcaoAux")
-
-const N = gets();
-let maiorNumeroPar = 0;
-let menorNumeroImpar = 1;
-
-for (let i = 0; i < N; i++) {
-  const numero = parseInt(gets());
+const pessoa = {
+  idade: 18
 }
 
-  if (numero % 2 == 0) {
-    if (numero > maiorNumeroPar) {
-      maiorNumeroPar = numero;
-    } else {
-        if (menorNumeroImpar === 0) {
-            menorNumeroImpar = numero
-        } else if (numero < menorNumeroImpar) {
-            menorNumeroImpar = numero;
-        }
-  }
-  
+const renan = {
+  nome: 'Renan',
+  idade: 30,
+  __proto__: pessoa
 }
+
+console.log(renan.idade)
